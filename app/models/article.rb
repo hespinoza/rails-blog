8a63fb01209c2dfd9ca4da6ca17cd1eeef960af2
Héctor Espinoza <Hector@MacBook-Pro-de-Hector.local>
2016-08-17 16:38:20 -0300

    Add user references to articles model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-
+	belongs_to :user
 	scope :from_new, -> { reorder(id: :desc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817072056) do
+ActiveRecord::Schema.define(version: 20160817185146) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "name"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160817072056) do
     t.string   "category"
     t.integer  "visits_count", default: 0
     t.string   "state",        default: "new"
+    t.integer  "user_id"
   end
+
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
