fa4c4627cdc420d79013c6cdb14883557f784aec
Héctor Espinoza <Hector@MacBook-Pro-de-Hector.local>
2016-08-18 02:29:26 -0300

    Modify articles show and view method

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,5 +1,16 @@
 CKEDITOR.editorConfig = function (config) {
   config.toolbar_mini = [
-    ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript", "Image", "Link", "Styles", "Format"],
+    { name: 'document',    items: [ 'Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+    { name: 'clipboard',   items: [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+    { name: 'editing',     items: [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+    { name: 'tools',       items: [ 'Maximize', 'ShowBlocks','-','About' ] }
+    '/',
+    { name: 'basicstyles', items: [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+    { name: 'paragraph',   items: [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+    { name: 'links',       items: [ 'Link','Unlink','Anchor' ] },
+    '/',
+    { name: 'styles',      items: [ 'Styles','Format','Font','FontSize' ] },
+    { name: 'colors',      items: [ 'TextColor','BGColor' ] },
+    { name: 'insert',      items: [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak' ] },
   ];
 }
